5103a111c13a14ba733e0d279fba29e371b6cd04
ANURAG CHAWLA <anuragchawla@ANURAGs-MacBook-Air.local>
2019-11-09 14:06:20 -0800

    Updates

---
+++ precache-manifest.942ee98ae9f349533dd9332aa4ce2dcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c2ae0e842f3566bc42c03ae73526c29",
+    "url": "/my-alpha/index.html"
+  },
+  {
+    "revision": "7028d7a0cec027844429",
+    "url": "/my-alpha/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a47510dc3c27ba1f4ce8",
+    "url": "/my-alpha/static/js/2.04b95750.chunk.js"
+  },
+  {
+    "revision": "7028d7a0cec027844429",
+    "url": "/my-alpha/static/js/main.4c9814b5.chunk.js"
+  },
+  {
+    "revision": "b3b112e2547619de2e31",
+    "url": "/my-alpha/static/js/runtime-main.78d96849.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/my-alpha/static/media/logo.25bf045c.svg"
+  }
+]);
